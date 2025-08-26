33880d0f27b22a209b4fae213ce744e46f00e178
Jaydevameta2003 <136778042+Jaydevameta2003@users.noreply.github.com>
2025-08-26 21:14:11 +0530

    Update nltk_setup.py

--- nltk_setup.py
+++ nltk_setup.py
@@ -1,13 +1,9 @@
 import nltk
-from textblob import download_corpora
 
-# Download required corpora
-nltk.download('punkt')
-nltk.download('punkt_tab')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('brown')
-nltk.download('wordnet')
-nltk.download('omw-1.4')
+def download_corpora():
+    nltk.download("brown")
+    nltk.download("wordnet")
+    nltk.download("omw-1.4")
 
-# For TextBlob
-download_corpora()
+if __name__ == "__main__":
+    download_corpora()
