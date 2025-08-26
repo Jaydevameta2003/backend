88d530bd3497a204c7616218500fabc896544d43
Jaydevameta2003 <136778042+Jaydevameta2003@users.noreply.github.com>
2025-08-26 21:11:12 +0530

    Update nltk_setup.py

--- nltk_setup.py
+++ nltk_setup.py
@@ -1,9 +1,13 @@
 import nltk
+from textblob import download_corpora
 
-# Download required corpora for TextBlob & NRCLex
+# Download required corpora
 nltk.download('punkt')
 nltk.download('punkt_tab')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('brown')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
+
+# For TextBlob
+download_corpora()
