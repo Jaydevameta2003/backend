b06d473f18b072ec1c3eb7335eb22cfefdc63567
Jaydevameta2003 <136778042+Jaydevameta2003@users.noreply.github.com>
2025-08-26 21:06:38 +0530

    Create nltk_setup.py

---
+++ nltk_setup.py
@@ -0,0 +1,9 @@
+import nltk
+
+# Download required corpora for TextBlob & NRCLex
+nltk.download('punkt')
+nltk.download('punkt_tab')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('brown')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
